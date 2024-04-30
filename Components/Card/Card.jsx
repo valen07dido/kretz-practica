db1b84f051bf83a8496838900b343061d4de294e
valen07dido <valendido69@gmail.com>
2024-04-30 14:17:55 -0300

    card done

---
+++ Components/Card/Card.jsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import styles from "./Card.module.css"
+const Card = ({img,title}) => {
+  return (
+    <div className={styles.container}>
+      <Image src={img} width={265} height={229.11} className={styles.image} alt={title}/>
+      <div className={styles.content}>
+      <h1 className={styles.title}>{title}</h1>
+      <h3 className={styles.button}>{"Ver Mas >"}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- app/mercado/page.jsx
+++ app/mercado/page.jsx
@@ -1,9 +1,11 @@
+import Card from '@/Components/Card/Card'
+import imagePrueba from "@/public/Report-NX-Visor-Bajo.jpg"
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      adadsadasdas
+      <Card img={imagePrueba} title={"Soporte"}/>
     </div>
   )
 }
