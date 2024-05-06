8a7aa0f270200fc52a07eb028c7748b5395ae67c
valen07dido <valendido69@gmail.com>
2024-05-06 14:28:00 -0300

    ajuste del carousel

--- Components/Carousel/Carousel.jsx
+++ Components/Carousel/Carousel.jsx
@@ -12,14 +12,17 @@ export default function Carousel({array}) {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    swipeToSlide: true,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 3000,
   };
 
   return (
-    <Slider {...settings} className={styles.slider}>
+    <Slider {...settings} className={styles.carousel}>
       {array.map((item, index) => (
         <div key={index} className={styles.cardContainer}>
-          <Card img={item.img} title={item.title} />
+          <Card img={item.img} title={item.title}  className={styles.cartas}/>
         </div>
       ))}
     </Slider>
