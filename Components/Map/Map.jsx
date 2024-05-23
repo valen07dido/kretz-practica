25f7a125f69baaf2fbba3c5a21d9d413339b090c
valen07dido <valendido69@gmail.com>
2024-05-23 16:40:43 -0300

    mapa y carousel inferior del home

--- Components/Map/Map.jsx
+++ Components/Map/Map.jsx
@@ -1,13 +1,45 @@
-import Image from 'next/image'
-import React from 'react'
-import map from "@/public/mapa.png"
-const Map = () => {
+import Image from "next/image";
+import React from "react";
+import map from "@/public/mapa.png";
+import styles from "./Map.module.css";
+const MapComponent = () => {
   return (
-    <div>
-        <h1>Nuestros Mercados</h1>
-    <Image src={map} width={1228.4}/>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Nuestros Mercados</h1>
+      <Image src={map} width={1200}/>
+      <div className={styles.box1}>
+        <h3 className={styles.title2}>
+          La empresa opera regularmente en los siguientes países:
+        </h3>
+        <div className={styles.box2}>
+          <div className={styles.chest1}>
+            <p className={styles.countries}>Arabia Saudita</p>
+            <p className={styles.countries}>Brasil</p>
+            <p className={styles.countries}>Bolivia</p>
+            <p className={styles.countries}>Chile</p>
+          </div>
+          <div className={styles.chest2}>
+            <p className={styles.countries}>Colombia</p>
+            <p className={styles.countries}>Costa Rica</p>
+            <p className={styles.countries}>Ecuador</p>
+            <p className={styles.countries}>Estados Unidos</p>
+          </div>
+          <div className={styles.chest3}>
+            <p className={styles.countries}>Ghana</p>
+            <p className={styles.countries}>Guatemala</p>
+            <p className={styles.countries}>Líbano</p>
+            <p className={styles.countries}>México</p>
+          </div>
+          <div className={styles.chest4}>
+            <p className={styles.countries}>Panamá</p>
+            <p className={styles.countries}>Paraguay</p>
+            <p className={styles.countries}>Perú</p>
+            <p className={styles.countries}>Uruguay</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Map
+export default MapComponent;
--- app/page.js
+++ app/page.js
@@ -2,10 +2,11 @@ import CardConoceMas from "@/Components/CardConoceMas/CardConoceMas";
 import styles from "./page.module.css";
 import Carousel from "@/Components/Carousel/Carousel";
 import img1 from "@/public/Balanza-Report-NX.png";
-import kiosko from "@/public/kiosko.png"
-import itegra from '@/public/itegra.png'
-import empresa from '@/public/empresa.png'
+import kiosko from "@/public/kiosko.png";
+import itegra from "@/public/itegra.png";
+import empresa from "@/public/empresa.png";
 import HorizontalCard from "@/Components/HorizontalCard/HorizontalCard";
+import MapComponent from "@/Components/Map/Map";
 
 export default function Home() {
   const arr = [
@@ -73,11 +74,43 @@ export default function Home() {
         ))}
       </div>
       <div className={styles.products}>
-      {horizontalArr.map((item, index) => (
-          <div key={index} >
-            <HorizontalCard img={item.img} text={item.text} title={item.title} orientation={item.orientation}/>
+        {horizontalArr.map((item, index) => (
+          <div key={index}>
+            <HorizontalCard
+              img={item.img}
+              text={item.text}
+              title={item.title}
+              orientation={item.orientation}
+            />
           </div>
         ))}
+      </div>
+      <div>
+      <div className={styles.carousel}>
+        <div>
+          <h1 className={styles.title2}>Novedades</h1>
+        </div>
+        <Carousel
+          array={[
+            {
+              img: "https://ethic.es/wp-content/uploads/2023/03/imagen.jpg",
+              title: "hola",
+            },
+            {
+              img: "https://ethic.es/wp-content/uploads/2023/03/imagen.jpg",
+              title: "hola",
+            },
+            {
+              img: "https://ethic.es/wp-content/uploads/2023/03/imagen.jpg",
+              title: "hola",
+            },
+          ]}
+          className={styles.carouselcomp}
+        />
+      </div>
+      </div>
+      <div className={styles.products}>
+        <MapComponent />
       </div>
     </main>
   );
