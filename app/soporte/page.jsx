f7b1a50c38aee45846e5b79c10d26a4a11fac28b
valentin di domenica <valendido69@gmail.com>
2024-05-27 15:46:42 -0300

    page soporte done

--- app/soporte/page.jsx
+++ app/soporte/page.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
-import styles from "./page.module.css"
+import React from "react";
+import styles from "./page.module.css";
+import Postventa from "@/public/Icons_buttons/Posventa.svg";
+import Garantias from "@/public/Icons_buttons/Garantias.svg";
+import AtencionAlCliente from "@/public/Icons_buttons/AtencionAlCliente.svg";
+import Tutoriales from "@/public/Icons_buttons/Tutoriales.svg";
+import ButtonSupport from "@/Components/ButtonSupport/ButtonSupport";
 const Page = () => {
+  const array = [
+    {
+      icon: "https://res.cloudinary.com/dpa8t14c2/image/upload/v1716833891/Kretz-Practica/Icons/mmtb1rgk9ymmrhpywfsj.svg",
+      text: "Garantias",
+    },
+    {
+      icon: "https://res.cloudinary.com/dpa8t14c2/image/upload/v1716833891/Kretz-Practica/Icons/ctuajnfrw4sgijgwhpoc.svg",
+      text: "Atencion al cliente",
+    },
+    {
+      icon: "https://res.cloudinary.com/dpa8t14c2/image/upload/v1716833891/Kretz-Practica/Icons/sykvzci3ymz8s4iqbn1r.svg",
+      text: "Tutoriales",
+    },
+    {
+      icon: "https://res.cloudinary.com/dpa8t14c2/image/upload/v1716833891/Kretz-Practica/Icons/xbj6vl90vyt0op22uitp.svg",
+      text: "Posventa",
+    },
+  ];
   return (
-    <div className={styles.header}>
-      <h1 className={styles.title}>Soporte</h1>
+    <div>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Soporte</h1>
+      </div>
+      <div className={styles.buttons}>
+        {array.map((item, index) => {
+          return <ButtonSupport icon={item.icon} text={item.text} />;
+        })}
+      </div>
     </div>
   );
 };
