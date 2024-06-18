e432b4ca9f05d3043784e005de9769de57ab152d
valen07dido <valendido69@gmail.com>
2024-06-18 13:32:20 -0300

    arreglo en la ruta de get y ajuste en tutorial

--- app/api/getModel/route.js
+++ app/api/getModel/route.js
@@ -1,11 +1,19 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
-export async function GET(request){
+export async function GET(request) {
     try {
-        const result=await sql`SELECT * FROM models;`
-        return NextResponse.json(result.rows,{status:200})
+        const randomSuffix = Math.random().toString(36).substring(7); 
+
+        const queryString = `
+            SELECT * FROM models
+            WHERE md5(id::text || '${randomSuffix}') = md5(id::text || '${randomSuffix}');
+        `;
+
+        const result = await sql.query(queryString);
+
+        return NextResponse.json(result.rows, { status: 200 });
     } catch (error) {
-        return NextResponse.json({error},{status:500})
+        return NextResponse.json({ error }, { status: 500 });
     }
 }
--- app/soporte/tutoriales/page.jsx
+++ app/soporte/tutoriales/page.jsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Card from "@/Components/Card/Card";
 import styles from "./page.module.css";
+import loadingimg from "@/public/loading.svg";
+import error from "@/public/error.png";
 import Image from "next/image";
 import BannerTutorial from "@/Components/BannerTutorial/BannerTutorial";
 import axios from "axios";
@@ -23,33 +25,44 @@ const Page = () => {
       });
   }, []);
 
-  return loading ? (
-    <p>cargando</p>
-  ) : (
+  return (
     <div className={styles.containers}>
       <div>
         <BannerTutorial />
       </div>
-      <div className={styles.grid}>
-        {array.map((item, index) => {
-          const path = item.name.split(" ").join("");
-          return (
-            <Link
-              href={`/soporte/tutoriales/${path}`}
-              className={styles.links}
-              key={index}
-            >
-              <div key={index} className={styles.cardContainer}>
-                <Card
-                  img={item.image}
-                  title={item.name}
-                  className={styles.cartas}
-                />
-              </div>
-            </Link>
-          );
-        })}
-      </div>
+      {loading ? (
+        <Image src={loadingimg} width={90} height={90} />
+      ) : (
+        <>
+          {array.length <= 0 ? (
+            <div className={styles.notfound}>
+              <Image src={error} width={300} height={300} />
+              <p className={styles.notfoundText}>Aun no existen datos...</p>
+            </div>
+          ) : (
+            <div className={styles.grid}>
+              {array.map((item, index) => {
+                const path = item.name.split(" ").join("");
+                return (
+                  <Link
+                    href={`/soporte/tutoriales/${path}`}
+                    className={styles.links}
+                    key={index}
+                  >
+                    <div key={index} className={styles.cardContainer}>
+                      <Card
+                        img={item.image}
+                        title={item.name}
+                        className={styles.cartas}
+                      />
+                    </div>
+                  </Link>
+                );
+              })}
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 };
