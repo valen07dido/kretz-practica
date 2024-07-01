161750f78ac2e66fd61e7fc786696a9eb7b6d0b7
valen07dido <valendido69@gmail.com>
2024-07-01 15:37:36 -0300

    trabajo en soporte posventa

--- app/soporte/posventa/page.jsx
+++ app/soporte/posventa/page.jsx
@@ -10,36 +10,45 @@ const page = () => {
           src="https://res.cloudinary.com/dpa8t14c2/image/upload/v1716833891/Kretz-Practica/Icons/ctuajnfrw4sgijgwhpoc.svg"
           width={46}
           height={49}
+          className={styles.img}
         />
       </div>
       <div className={styles.box2}>
         <div className={styles.chest1}>
-          <h3>
+          <h3 className={styles.textBold}>
             Hacer productos para que ayuden a las personas es un concepto que
             empieza mucho antes del producto mismo y que no termina.
           </h3>
-          <p>
+          <p className={styles.text}>
             Entender este pensamiento es prioritario a la hora de definir los
             alcances del compromiso con la gente.
           </p>
-          <p>
+          <p className={styles.text}>
             En ese sentido, Kretz desarrolló un servicio de posventa que abarca
             más de setenta servicios técnicos en todo el país, y otro importante
             número en los países donde exporta sus productos.
           </p>
-          <p>
+          <p className={styles.text}>
             Para dar respuesta en tiempo y forma, porque la tecnología no es
             perfecta y la idea es brindar soluciones.
           </p>
         </div>
         <div className={styles.chest2}>
-          <h1>70</h1>
-          <p>Servicios Técnicos en todo el pais</p>
-          <p>También disponible en países de exportación</p>
-          <hr/>
-          <p>Para mayor información contáctese con nosotros</p>
-          <h2>0341 492 7400</h2>
-          <p>líneas rotativas</p>
+          <div className={styles.services}>
+            <h1 className={styles.serviceNumber}>70</h1>
+            <p className={styles.serviceText}>
+              Servicios Técnicos en todo el pais
+            </p>
+          </div>
+          <p className={styles.text2}>
+            También disponible en países de exportación
+          </p>
+          <hr className={styles.line} />
+          <p className={styles.text2}>
+            Para mayor información contáctese con nosotros
+          </p>
+          <h2 className={styles.number}>0341 492 7400</h2>
+          <p className={styles.text2}>líneas rotativas</p>
         </div>
       </div>
     </div>
