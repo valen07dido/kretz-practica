295784e278d43b29f3cb10a845b93e5fa8802136
valentin di domenica <valendido69@gmail.com>
2024-07-18 20:08:57 -0300

    funcionalidad de boton

--- Components/CardConoceMas/CardConoceMas.jsx
+++ Components/CardConoceMas/CardConoceMas.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./CardConoceMas.module.css";
+import Link from "next/link";
 import ButtonConoceMas from "../ButtonConoceMas/ButtonConoceMas";
 const CardConoceMas = ({img, text}) => {
   return (
@@ -8,7 +9,9 @@ const CardConoceMas = ({img, text}) => {
       <Image src={img} width={400} height={600} alt={text} />
       <div className={styles.container2}>
         <h1 className={styles.name}>{text}</h1>
+        <Link href={`/productos/${text}`} className={styles.link}>
         <ButtonConoceMas />
+        </Link>
       </div>
     </div>
   );
