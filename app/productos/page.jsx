55da7055e3bc1673e1bde0f42a2a8360aba07dd4
valentin di domenica <valendido69@gmail.com>
2024-07-18 20:00:25 -0300

    mapa en productos

--- app/productos/page.jsx
+++ app/productos/page.jsx
@@ -1,10 +1,12 @@
 "use client";
+import MapComponent from "@/Components/Map/Map";
 import Link from "next/link";
 import React from "react";
 const page = () => {
   return (
     <div>
       <Link href={"/productos/prueba"}>clickea</Link>
+      <MapComponent/>
     </div>
   );
 };
