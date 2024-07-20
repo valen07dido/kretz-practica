8a7d298fcdcb7374053f51ca407aabd0aa4491c7
valentin di domenica <valendido69@gmail.com>
2024-07-20 18:17:52 -0300

    responsive v:1.0

--- Components/HorizontalCard/HorizontalCard.jsx
+++ Components/HorizontalCard/HorizontalCard.jsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import React from "react";
-import styles from "./HorizontalCard.module.css"
+import styles from "./HorizontalCard.module.css";
+
 const HorizontalCard = ({ img, title, text, orientation }) => {
   return (
-    <div className={orientation==="D"?styles.containerD:styles.containerI}>
+    <div className={orientation === "D" ? styles.containerD : styles.containerI}>
       <div className={styles.containerText}>
         <h1 className={styles.title}>{title}</h1>
         <p className={styles.text}>{text}</p>
         <h2 className={styles.button}>{"Ver Más >"}</h2>
       </div>
-      <Image src={img} width={550} height={417} alt={title}/>
+      <Image src={img} width={550} height={417} alt={title} />
     </div>
   );
 };
