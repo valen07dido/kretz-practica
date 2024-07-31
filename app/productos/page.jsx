7a1c17d8e9a96cad19818c7d2b9c2579385bc2be
valentin di domenica <valendido69@gmail.com>
2024-07-30 21:12:39 -0300

    empezando productos

--- app/productos/page.jsx
+++ app/productos/page.jsx
@@ -1,11 +1,14 @@
 "use client";
 import MapComponent from "@/Components/Map/Map";
 import Link from "next/link";
+import styles from "./page.module.css"
 import React from "react";
 const page = () => {
   return (
     <div>
-      <Link href={"/productos/prueba"}>clickea</Link>
+      <h1 className={styles.title}>
+          productos
+      </h1>
       <MapComponent/>
     </div>
   );
