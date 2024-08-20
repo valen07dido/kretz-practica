eca34d06c75c91c03dc361a5c0ed40f03c1067c5
valen07dido <valendido69@gmail.com>
2024-08-19 23:00:08 -0300

    ruta de usuario y creacion de middelware

--- app/api/getUsers/route.js
+++ app/api/getUsers/route.js
@@ -5,7 +5,7 @@ export async function GET(request) {
   try {
     const result = await sql`SELECT id, email, name, rol, created_at FROM "Users"`;
     
-    console.log(result.rows); // Esto te ayudará a ver cuántos registros estás recibiendo
+    console.log(result.rows); 
     
     const response = NextResponse.json(result.rows, { status: 200 });
 
